ca448ec4741f9ffcb8ca16c355d6b27dcded3231
MohanChakkani <mohanchakkani263@gmail.com>
2023-04-24 23:15:26 +0530

    Decryption done

--- CapstoneRestTemplate/src/main/java/com/capstone/project/entity/Employee.java
+++ CapstoneRestTemplate/src/main/java/com/capstone/project/entity/Employee.java
@@ -1,9 +1,16 @@
 package com.capstone.project.entity;
 
 import java.sql.Date;
+import java.time.LocalDate;
 
 
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.time.LocalDate;
+import java.util.Base64;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 
 
 
@@ -11,59 +18,64 @@ import java.sql.Date;
 public class Employee {
 	
 	
-    //@GeneratedValue(strategy = GenerationType.IDENTITY)
-    //@Column(name = "EmployeeID")
-    private int employeeId;
     
-    //@Column(name = "EmployeeName")
-    private String employeeName;
-    
-    //@Column(name = "DateOfBirth")
-    private Date dateOfBirth;
-    
-    public Employee() {
-		// TODO Auto-generated constructor stub
-    	super();
+	private long employeeId;
+	private String employeeName;
+	private LocalDate dateOfBirth;
+	
+	public Employee() {
+		super();
 	}
-    
+    private static final String ALGORITHM = "AES";
+    private static final String KEY = "mysecretkey12345";
+	public Employee(long employeeId, String employeeName, String dateOfBirth) {
+		super();
+		this.employeeId = employeeId;
+		this.employeeName = employeeName;
+		this.dateOfBirth = LocalDate.parse(decrypt(dateOfBirth, KEY));
+	}
+	
 
-	public int getEmployeeId() {
+	
+	 private static String decrypt(String encryptedText, String key) {
+	        try {
+	            Key aesKey = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), ALGORITHM);
+	            Cipher cipher = Cipher.getInstance(ALGORITHM);
+	            cipher.init(Cipher.DECRYPT_MODE, aesKey);
+	            byte[] decrypted = cipher.doFinal(Base64.getDecoder().decode(encryptedText));
+	            return new String(decrypted, StandardCharsets.UTF_8);
+	        } catch (Exception e) {
+	            throw new RuntimeException("Error decrypting text", e);
+	        }
+	    }
+
+	public long getEmployeeId() {
 		return employeeId;
+	}
+
+	public void setEmployeeId(long employeeId) {
+		this.employeeId = employeeId;
 	}
 
 	public String getEmployeeName() {
 		return employeeName;
 	}
 
-	public Date getDateOfBirth() {
-		return dateOfBirth;
-	}
-
-	public void setEmployeeId(int employeeId) {
-		this.employeeId = employeeId;
-	}
-
 	public void setEmployeeName(String employeeName) {
 		this.employeeName = employeeName;
 	}
 
-	public void setDateOfBirth(Date dateOfBirth) {
-		this.dateOfBirth = dateOfBirth;
+	public LocalDate getDateOfBirth() {
+		return dateOfBirth;
 	}
-	
+
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = LocalDate.parse(decrypt(dateOfBirth, KEY));
+	}
 
 	@Override
 	public String toString() {
 		return "Employee [employeeId=" + employeeId + ", employeeName=" + employeeName + ", dateOfBirth=" + dateOfBirth
 				+ "]";
 	}
-
-	public Employee(int employeeId, String employeeName, Date dateOfBirth) {
-		super();
-		this.employeeId = employeeId;
-		this.employeeName = employeeName;
-		this.dateOfBirth = dateOfBirth;
-	}
-    
-
 }
